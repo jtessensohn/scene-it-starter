28131642650898f3a40f2a8eb01378aa91cbb839
Jared Tessensohn <jtessensohn@yahoo.com>
2021-02-15 16:06:26 -0600

    finished part 3 and some of part 4

--- index.js
+++ index.js
@@ -1,14 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+        const searchForm = document.getElementById('search-form')
+        movieData = ""
+        searchForm.addEventListener('submit', function(e) {
+            // event listener code here
+            e.preventDefault();
+            const searchString = document.getElementById('search-bar').value
+            const urlEncodedSearchString = encodeURIComponent(searchString);
+            axios.get("http://www.omdbapi.com/?apikey=59354c85&s=" + urlEncodedSearchString).then( (response) => {
+                movieData = response.data.Search
+                cardTitle.innerHTML = renderMovies(response.data.Search)
+        });
+
+
+    })
+})
+
 function renderMovies(movieArray) {
-    const movieHtmlArray = movieArray.map(function(currentMovie) {
-        retun `<div>
-            <h2>${currentMovie.Title}</h2>
+    let movieHtmlArray = movieArray.map(function(currentMovie) {
+        return `<div class="movie col mb-4">
+        <div class="card" style="width: 18rem">
+        <img src="${currentMovie.Poster}" alt="no image" class="card-img-top">
+        <div class="card-body">
+        <h5>${currentMovie.Title}</h5>
+        <p class="card-text">${currentMovie.Year}</p>
+        <a href="#" onclick= "saveToWatchList('${currentMovie.imdbID}')" class="btn btn-primary add-button">Add movie</a>
+        </div>
+        </div>
         </div>`
     });
-    return movieHtmlArray.join('');
+    return movieHtmlArray.join("");
 }
 
-const moviesContainer = document.getElementsByClassName('#movies-container')
+function saveToWatchList(imdbID) {
+    let movie = movieData.find((currentMovie) => {
+            return currentMovie.imdbID == imdbID;
+        });
+        let watchlistJSON = localStorage.getItem('watchlist');
+        let watchlist = JSON.parse(watchlistJSON);
+        if (watchlist == null) {
+            watchlist = []
+        }
+        watchlist.push(movie);
+        watchlistJSON = JSON.stringify(watchlist);
+        localStorage.setItem('watchlist', watchlistJSON);
+        console.log(imdbID)
+    }
 
-document.addEventListener('DOMContentLoaded', function() {
-    moviesContainer.innerHTML = renderMovies(movieData)
-})
+
+const addButton = document.querySelector('.add-button')
+const cardTitle = document.querySelector('.movies-container')
+
+
+
+
---
+++ watchlist.js
@@ -0,0 +1,39 @@
+function renderMovies(movieArray) {
+    let movieHtmlArray = movieArray.map(function(currentMovie) {
+        return `<div class="movie col mb-4">
+        <div class="card" style="width: 18rem">
+        <img src="${currentMovie.Poster}" alt="no image" class="card-img-top">
+        <div class="card-body">
+        <h5>${currentMovie.Title}</h5>
+        <p class="card-text">${currentMovie.Year}</p>
+        <a href="#" onclick="removeWatchlist('${currentMovie.imdbID}')" class="btn btn-danger remove-button">Remove</a>
+        </div>
+        </div>
+    </div>`
+    });
+    console.log(movieHtmlArray)
+    return movieHtmlArray.join("");
+}
+
+function removeWatchlist(imdbID) {
+        let watchlistJSON = localStorage.getItem('watchlist');
+        let watchlist = JSON.parse(watchlistJSON);
+        if (watchlist == null) {
+            watchlist = []
+        }
+        let newWatchlist = watchlist.filter(movie => movie.imdbID != imdbID);
+        cardTitle.innerHTML = (`${renderMovies(newWatchlist)}`)
+        watchlistJSON = JSON.stringify(newWatchlist);
+        localStorage.setItem('watchlist', watchlistJSON);
+        
+    }
+
+const cardTitle = document.querySelector('.movies-container')
+const removeButton = document.getElementsByClassName('remove-button')
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    let watchlistJSON = localStorage.getItem('watchlist')
+    let watchlist = JSON.parse(watchlistJSON)
+    cardTitle.innerHTML = (`${renderMovies(watchlist)}`)
+})
